60a6e0688429c1b4745968bc99c62bac5fccbba6
Uriel Santillán Paz <santillanpazuriel@gmail.com>
2023-01-16 10:59:13 -0600

    Add:Ruteo de login y productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductosComponent } from './components/productos/productos.component';
+
+const rutas:Routes=[
+  {
+    path:'', redirectTo:'login', pathMatch:'full'
+  },
+  {
+    path:'login', component:LoginComponent
+  },
+  {
+    path:'productos', component:ProductosComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
