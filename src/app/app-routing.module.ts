b868472a0a5cc87ee92e8a6dc1470e4cde97b8b1
Uriel Santillán Paz <santillanpazuriel@gmail.com>
2023-01-18 21:40:24 -0600

    Add:Interfaz de login y de los productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent/*, canActivate: [jwtGuard]*/ },
+  { path: 'productos', component: ProductosComponent/*, canActivate: [jwtGuard]*/ },
+  { path: '**', redirectTo: '/login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { ProductosComponent } from './components/productos/productos.component';
-
-const rutas:Routes=[
-  {
-    path:'', redirectTo:'login', pathMatch:'full'
-  },
-  {
-    path:'login', component:LoginComponent
-  },
-  {
-    path:'productos', component:ProductosComponent
-  }
-]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    ProductosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
