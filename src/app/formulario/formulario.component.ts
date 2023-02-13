f41af2b458d199a3fbb3a28b851af0b231d09588
Uriel Santillán Paz <santillanpazuriel@gmail.com>
2023-02-13 09:36:27 -0600

    Add:Formulario reactivo de productos

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { WeekDay } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators,AbstractControl, FormBuilder } from '@angular/forms';
 
   @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
-  
+export class FormularioComponent implements OnInit {
+  public producto!: FormGroup;
+  constructor(private formulario:FormBuilder){}
+
+  createForm(){
+    this.producto = this.formulario.group({
+      fecha: [new Date().toLocaleDateString(),Validators.required],
+      productos: ["",Validators.required],
+      piezas: ["",Validators.required],
+  });
+  }
+
+  resetForm(): void {
+    this.producto.reset();
+    this.producto.value.id();
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
 }
