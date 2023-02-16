4e0d8f841583094653a10f6698499d4c728fa696
Uriel Santillán Paz <santillanpazuriel@gmail.com>
2023-02-16 14:48:50 -0600

    Add:Base para realizar calculos en formulario

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -2,27 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators,AbstractControl, FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { SouterCalculoAceroService } from '../../services/souter-calculoAcero-service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-  @Component({
+@Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
   public producto!: FormGroup; // producto corresponde al nombre del formulario
+  resultado$!: Observable<number>; // resultado$ corresponde al resultado de dicha operacion al calcular el acero
   public listaproductos:any=[]; // listaproductos corresponde al nombre de la lista que se va a mostrar (maneja el json completo)
   public listaproducto:any; // listaproducto corresponde al nombre del producto que se va a mostrar (id)
   public listaNoEstaVacia(): boolean { // listaNoEstaVacia corresponde al nombre de la funcion que se va a utilizar en el html para mostrar la lista
     return this.listaproductos.length > 0;
   }
   constructor(private formulario:FormBuilder,private SouterCalculoAceroService:SouterCalculoAceroService, private route:ActivatedRoute){}
-
+  
   createForm(){
     this.producto = this.formulario.group({
       fecha: [new Date().toLocaleDateString(),Validators.required], // fecha corresponde al campo fecha que es lo que va a ingresar el ususario
-      productos: ["",Validators.required], // productos corresponde al campo producto que es lo que va a ingresar el ususario
       piezas: ["",Validators.required], // piezas corresponde al campo piezas que es lo que va a ingresar el ususario
-  });
+      // numero1: ["",Validators.required],
+      // numero2: ["",Validators.required],
+      resultado: ''
+    });
+    this.resultado$ = this.producto.valueChanges
+    .pipe(map(({ numero1, numero2 }) => numero1 + numero2));
   }
 
   resetForm(): void {
